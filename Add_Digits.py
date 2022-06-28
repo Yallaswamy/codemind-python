47451d94756cce1fd17f674d7cd3c99c1001d588
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-06-28 19:54:56 +0530

    def digit(n):     res=0     while(n!=0):         r=n%10         res=res+r         n=n//10     if(res>9):         return digit(res)     else:         return res n=int(input()) k=digit(n) print(k)

--- Add_Digits.py
+++ Add_Digits.py
@@ -1,27 +1,13 @@
-def adddigits(n):
-    sum=0
-    r=0
+def digit(n):
+    res=0
     while(n!=0):
         r=n%10
-        sum=sum+r
+        res=res+r
         n=n//10
-    return sum
-        
+    if(res>9):
+        return digit(res)
+    else:
+        return res
 n=int(input())
-#print(n)
-k=adddigits(n)
-#print(k)
-for i in range(0,10):
-    if(k==i):
-        print(i)
-        break
-else:
-    k=adddigits(k)
-    s=0
-    while(k!=0):
-        m=k%10
-        s=s+m
-        k=k//10
-    print(s)
-
-
+k=digit(n)
+print(k)
