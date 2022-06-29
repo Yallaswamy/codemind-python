e7aa5e80e742693d8871ed5dcb3fb7f91ab0a5a1
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-06-29 13:33:05 +0530

    n=int(input()) a=list(map(int,input().split())) #print(a) c=[] p=0 for i in a:     b=len(str(i))     c.append(b) for i in a:     if(min(c)==len(str(i))):         p=p+1 print(p)

--- Minimum_digit_elements.py
+++ Minimum_digit_elements.py
@@ -1,17 +1,14 @@
-n=input()
-c=list(map(int,input().split()))
-#print(c)
-d=0
-m=100
-n=0
-for i in c:
-    k=len(str(i))
-    if(m>k):
-        m=k
-for i in c:
-    k=len(str(i))
-    if(m==k):
-        n=n+1
-print(n)
-        
+n=int(input())
+a=list(map(int,input().split()))
+#print(a)
+c=[]
+p=0
+for i in a:
+    b=len(str(i))
+    c.append(b)
+for i in a:
+    if(min(c)==len(str(i))):
+        p=p+1
+print(p)
+    
 
