8a1cca7d9675fc6f7952d2ee4cfa1a6bc8137c9a
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-06-29 20:31:34 +0530

    def prime(n):     c=0     for i in range(2,int(n**0.5)+1):         if(n%i==0):             c=c+1     if(c==0):         return 1     else:         return 0 n=int(input()) n1=int(input()) s=n+n1 i=1 while(1):     if(prime(s+i)):         print(i)         break     i=i+1

--- Battle_of_Primes.py
+++ Battle_of_Primes.py
@@ -1,18 +1,20 @@
 def prime(n):
-    count=0
+    c=0
     for i in range(2,int(n**0.5)+1):
-        if n%i==0:
-            count+=1
-    if count==0:
+        if(n%i==0):
+            c=c+1
+    if(c==0):
         return 1
     else:
         return 0
-a=int(input())
-b=int(input())
-s=a+b
+n=int(input())
+n1=int(input())
+s=n+n1
 i=1
 while(1):
     if(prime(s+i)):
         print(i)
         break
     i=i+1
+
+        
