e6136d8e36c62933e639c8c76cdde09f716fb2c8
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-06-30 23:11:21 +0530

    n=int(input()) a=list(map(int,input().split())) print(max(a))

--- maximum_of_an_array.py
+++ maximum_of_an_array.py
@@ -1,9 +1,3 @@
-n=int(input()) 
+n=int(input())
 a=list(map(int,input().split()))
-max=0
-min=a[0]
-for i in range (1,n):#for i in a:
-    if(a[i]>min):    #if i<m:
-        min=a[i]     #m=i
-print(min)
-
+print(max(a))
