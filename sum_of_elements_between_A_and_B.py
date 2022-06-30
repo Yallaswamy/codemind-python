c8cd9004ea7428e739ad7267436331e46e7b821e
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-06-30 22:47:45 +0530

    n=int(input()) m=list(map(int,input().split())) a,b=map(int,input().split()) d=[] s=0 for i in m:     if(i in range(a,b+1)):         d.append(i) for i in d:     s=s+i print(s)

--- sum_of_elements_between_A_and_B.py
+++ sum_of_elements_between_A_and_B.py
@@ -1,19 +1,11 @@
 n=int(input())
-a=list(map(int,input().split()))
-#print(a)
-x,y=map(int,input().split())
-#print(x,y)
-b=[]
-sum=0
-c=0
-for i in a:
-    if i in range(x,y+1):
-        b.append(i)
-        c=c+1
-#print(b)
-if(c==0):
-    print(-1)
-else:
-    for i in b:
-        sum=sum+i
-print(sum)
+m=list(map(int,input().split()))
+a,b=map(int,input().split())
+d=[]
+s=0
+for i in m:
+    if(i in range(a,b+1)):
+        d.append(i)
+for i in d:
+    s=s+i
+print(s)
