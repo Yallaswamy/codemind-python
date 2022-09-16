6b6723ed780e547d65a0f93a8cea630b5cd91eda
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-09-16 19:49:00 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     if i not in b:         b.append(i) for i in b:     print(i,a.count(i),end=" ")

--- Element_and_Count.py
+++ Element_and_Count.py
@@ -2,11 +2,7 @@ n=int(input())
 a=list(map(int,input().split()))
 b=[]
 for i in a:
-    a.count(i)
-    for i in a:
-        if( i not in b):
-            b.append(i)
+    if i not in b:
+        b.append(i)
 for i in b:
     print(i,a.count(i),end=" ")
-        
-    
