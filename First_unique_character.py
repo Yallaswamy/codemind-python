8a622f5262deea9be8977a0d1402039e50951764
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-03 18:22:14 +0530

    n=input() n=n.lower() z=0 for i in n:     if(n.count(i)==1 and i!=" "):         print(i)         z=1         break if(z==0):     print("-1")

--- First_unique_character.py
+++ First_unique_character.py
@@ -1,11 +1,10 @@
 n=input()
-n=n.split()
-n="".join(n)
 n=n.lower()
+z=0
 for i in n:
-    if(n.count(i)==1):
+    if(n.count(i)==1 and i!=" "):
         print(i)
+        z=1
         break
-else:
-    print(-1)
-
+if(z==0):
+    print("-1")
