b196af5892d31e0136cc236c6294e41c02e8a048
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-04 11:23:23 +0530

    n=input() n=n.split() for i in n:     print(len(i),end=" ")

--- characters_in_each_word.py
+++ characters_in_each_word.py
@@ -1,9 +1,4 @@
 n=input()
-#print(n)
-c=0
-for i in range(len(n)):
-    c=c+1
-    if(n[i]==" "):
-        print(c-1,end=" ")
-        c=0
-print(c,end=" ")
+n=n.split()
+for i in n:
+    print(len(i),end=" ")
