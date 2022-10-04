38ab60bb7cde992e211fc8ec25a854ef55383c42
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-04 11:21:37 +0530

    n=input() n=n.split() m=0 for i in n:     if(len(i)>m):         m=len(i) print(m)

--- maximum_word_lengh.py
+++ maximum_word_lengh.py
@@ -1,23 +1,7 @@
 n=input()
-m=n.split()
-c=0
-k=0
-#print(m)
+n=n.split()
+m=0
 for i in n:
-    if(i==" "):
-        k=c
-        c=0
-    else:
-        c=c+1
-for i in n:
-    if n in m:
-        print(c)
-        break
-    else:
-        if(k>c):
-            print(k)
-            break
-        else:
-            print(c)
-            break
-
+    if(len(i)>m):
+        m=len(i)
+print(m)
