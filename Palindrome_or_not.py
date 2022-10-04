63f946de904402946c8f71c3457806ec9b7ba0f9
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-04 09:12:35 +0530

    n=input().lower() if(n==n[::-1]):     print(True) else:     print(False)

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -1,11 +1,5 @@
-n=input()
-#print(n)
-n=n.lower()
-n=n.split()
-for i in n:
-    m=i
-    p=i[::-1]
-    if(m==p):
-        print(True)
-    else:
-        print(False)
+n=input().lower()
+if(n==n[::-1]):
+    print(True)
+else:
+    print(False)
