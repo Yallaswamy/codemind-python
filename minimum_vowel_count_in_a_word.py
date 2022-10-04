81f63f500b64c00e7e94902673f0f6bd560e8d4b
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-04 10:25:42 +0530

    n=input().lower() n=n.split() a="aeiou" ma=100 for i in n:     c=0     for j in i:        if(j in a):            c=c+1     if(ma>c):         ma=c print(ma)

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,15 @@
+n=input().lower()
+n=n.split()
+a="aeiou"
+ma=100
+for i in n:
+    c=0
+    for j in i:
+       if(j in a):
+           c=c+1
+    if(ma>c):
+        ma=c
+print(ma)
+           
+       
+       
