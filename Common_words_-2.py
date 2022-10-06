14e4578343f57e68a29874d9a12eeee2e8b172bd
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-06 19:59:22 +0530

    s1=input().lower() s2=input().lower() s1=s1.split() s2=s2.split() a=[] b=[] c=0 for i in s2:     if(s2.count(i)==1):         a.append(i) for i in s1:     if(s1.count(i)==1):         b.append(i) for i in a:     if(i in b):         c=c+1 print(c)

--- Common_words_-2.py
+++ Common_words_-2.py
@@ -2,17 +2,18 @@ s1=input().lower()
 s2=input().lower()
 s1=s1.split()
 s2=s2.split()
-s=[]
-se=[]
+a=[]
+b=[]
+c=0
 for i in s2:
     if(s2.count(i)==1):
-        s.append(i)
+        a.append(i)
 for i in s1:
     if(s1.count(i)==1):
-        se.append(i)
-        
-c=0
-for i in se:
-    if( i in s):
+        b.append(i)
+for i in a:
+    if(i in b):
         c=c+1
 print(c)
+
+        
