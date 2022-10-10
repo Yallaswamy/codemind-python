7af6e628ce37be9138bde35dd61907b8fd6ebf32
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-10 20:27:48 +0530

    n=input() n=n.lower() s=len(n) v="aeiou" i=0 j=s-1 c=0 while i<=j:     if n[i]==' ' or n[j]==' ':         i+=1         j-=1         continue     elif n[i] in v and n[j] not in v:         c+=1     elif n[i] not in v and n[j] in v:         c+=1     i+=1     j-=1 print(c)

--- count_vowel_consonent_pairs.py
+++ count_vowel_consonent_pairs.py
@@ -1,10 +1,19 @@
-n=input().lower()
-n=list(n)
+n=input()
+n=n.lower()
+s=len(n)
 v="aeiou"
-c="bcdfghjklmnpqrstvwxyz"
-c1=0
-for i in range(len(n)):
-    if((n[i] in v and n[len(n)-1-i] in c) or (n[i] in c and n[len(n)-1-i] in v)):
-        c1=c1+1
-print(c1//2)
-        
+i=0
+j=s-1
+c=0
+while i<=j:
+    if n[i]==' ' or n[j]==' ':
+        i+=1
+        j-=1
+        continue
+    elif n[i] in v and n[j] not in v:
+        c+=1
+    elif n[i] not in v and n[j] in v:
+        c+=1
+    i+=1
+    j-=1
+print(c)
