5c164e213c86b717bb8fff3a92043e8a0be32e11
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-10 10:15:38 +0530

    n=input().lower() f=0 for i in range(len(n)-1):     for j in range(i+1,len(n)-1):         if(n[i]==n[j] and i!=j):             f=1             break if(f==0):     print(True) else:     print(False)

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,11 +1,11 @@
-n=input()
-c=0
-x=len(n)
-for i in n:
-    if(n.count(i)==1):
-        c=c+1
-if(x==c):
+n=input().lower()
+f=0
+for i in range(len(n)-1):
+    for j in range(i+1,len(n)-1):
+        if(n[i]==n[j] and i!=j):
+            f=1
+            break
+if(f==0):
     print(True)
 else:
     print(False)
-        
