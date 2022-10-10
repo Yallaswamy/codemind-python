31b78e7d9fc32c726000767e7cb2761dfbf80c5f
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-10 20:04:28 +0530

    n=input().lower() n=n.split() a="aeiou" ma=0 e=0 for i in n:     c=0     for j in i:         if(j in a):             c=c+1     if(c>ma):         ma=c for i in n:     c=0     for j in i:         if(j in a):             c=c+1     if(ma==c):         e=e+1 print(e)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,23 @@
+n=input().lower()
+n=n.split()
+a="aeiou"
+ma=0
+e=0
+for i in n:
+    c=0
+    for j in i:
+        if(j in a):
+            c=c+1
+    if(c>ma):
+        ma=c
+for i in n:
+    c=0
+    for j in i:
+        if(j in a):
+            c=c+1
+    if(ma==c):
+        e=e+1
+print(e)
+        
+    
+            
