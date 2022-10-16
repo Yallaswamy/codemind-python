b0f8702a1a37da197bfb7c07a9eaf652126bdadf
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-16 09:51:02 +0530

    n=int(input()) a=list(map(int,input().split())) #print(a) c=0 d=0 for i in range(0,n-1,2):     if(a[i]>a[i+1]):         c=c+1 for i in range(0,n-1,2):     if(a[i]<a[i+1]):         d=d+1
    #print(c,d)
    if(c==n//2 or d==n//2):
        print("yes")
    else:
        print("no")

--- Wave_array.py
+++ Wave_array.py
@@ -1,18 +1,17 @@
 n=int(input())
 a=list(map(int,input().split()))
+#print(a)
 c=0
-
+d=0
 for i in range(0,n-1,2):
-    if(a[0]<a[1]):
-        if(a[i]<a[i+1]):
-            c=c+1
-    else:
-        if(a[i]>a[i+1]):
-           c=c+1
-        
-if(c==n//2):
+    if(a[i]>a[i+1]):
+        c=c+1
+for i in range(0,n-1,2):
+    if(a[i]<a[i+1]):
+        d=d+1
+
+#print(c,d)
+if(c==n//2 or d==n//2):
     print("yes")
 else:
     print("no")
-        
-    
