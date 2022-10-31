a4f63a4282399ba6c74d42f173bc0531b0a4b5f2
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-10-31 10:11:13 +0530

    n,m=map(int,input().split()) a=[] for i in range(n):     l=list(map(int,input().split()))     a.append(l) sm=su=0 for i in range(0,len(a),2):     for j in a[i]:         sm+=j for i in range(1,len(a),2):     for j in a[i]:         su+=j print(sm,su)

--- Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.py
@@ -1,13 +1,14 @@
 n,m=map(int,input().split())
 a=[]
 for i in range(n):
-    b=list(map(int,input().split()))
-    a.append(b)
-s=s1=0
-for i in range(n):
-    for j in range(m):
-        if(i%2==0):
-            s=s+a[i][j]
-        else:
-            s1=s1+a[i][j]
-print(s,s1)
+    l=list(map(int,input().split()))
+    a.append(l)
+sm=su=0
+for i in range(0,len(a),2):
+    for j in a[i]:
+        sm+=j
+for i in range(1,len(a),2):
+    for j in a[i]:
+        su+=j
+print(sm,su)
+
