b216865443929d7bc00f8ac8f7fc7502d7fea639
swamyyalla <97425775+Yallaswamy@users.noreply.github.com>
2022-11-09 09:31:07 +0530

    def rev(n):     n=n[::-1]     return n n=input() n=n.split() a=[] for i in range(len(n)):     if(i%2==0):         k=rev(n[i])         a.append(k)     else:         a.append(n[i])
    print(*a)

--- reverse_even_positional_words.py
+++ reverse_even_positional_words.py
@@ -1,18 +1,14 @@
+def rev(n):
+    n=n[::-1]
+    return n
 n=input()
 n=n.split()
-c=0
-x=0
-q=""
-for i in n:
-    c+=1
-#print(c)
-for i in n:
-    if x<c:
-        if x%2==0:
-            q+=i[::-1]
-        else:
-            q+=i
-    if x+1<c:
-        q+=' '
-    x+=1
-print(q)   
+a=[]
+for i in range(len(n)):
+    if(i%2==0):
+        k=rev(n[i])
+        a.append(k)
+    else:
+        a.append(n[i])
+    
+print(*a)
